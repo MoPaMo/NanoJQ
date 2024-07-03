d76a893d0cc559e69ec682415432f40774874340
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2024-07-04 00:25:45 +0200

    update jq with .on

--- nanojq.js
+++ nanojq.js
@@ -7,7 +7,7 @@
       typeof selector === "string"
         ? document.querySelectorAll(selector)
         : [].concat(selector);
-    return this;
+    return this.elements.length === 1 ? this.elements[0] : this.elements;
   }
 
   NanoJQ.prototype = {
@@ -19,44 +19,21 @@
       this.elements.forEach((el) => (el.style.display = ""));
       return this;
     },
-  };
-
-  const handler = {
-    get: function (target, prop) {
-      if (prop in target) {
-        return target[prop];
-      }
-      return function (...args) {
-        let results = target.elements.map((el) => {
-          if (typeof el[prop] === "function") {
-            return el[prop](...args);
-          } else {
-            return el[prop];
-          }
-        });
-
-        // If the result is a DOM element or a list of DOM elements, wrap it in NanoJQ
-        if (
-          results.some((r) => r instanceof Element || r instanceof NodeList)
-        ) {
-          return new Proxy(new NanoJQ(results.flat()), handler);
-        }
-
-        return results.length > 1 ? results : results[0];
-      };
-    },
-    set: function (target, prop, value) {
-      target.elements.forEach((el) => {
-        el[prop] = value;
-      });
-      return true;
+    on: function (event, callback) {
+      this.elements.forEach((el) => el.addEventListener(event, callback));
+      return this;
     },
   };
 
-  function createNanoJQ(selector) {
-    return new Proxy(new NanoJQ(selector), handler);
-  }
+  // Extend element prototypes
+  Element.prototype.hide = NanoJQ.prototype.hide;
+  Element.prototype.show = NanoJQ.prototype.show;
+  Element.prototype.on = NanoJQ.prototype.on;
 
-  window.$ = createNanoJQ;
+  NodeList.prototype.hide = NanoJQ.prototype.hide;
+  NodeList.prototype.show = NanoJQ.prototype.show;
+  NodeList.prototype.on = NanoJQ.prototype.on;
+
+  window.$ = NanoJQ;
   window._ = console.log.bind(console);
 })(window);
