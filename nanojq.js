700f9b12f6fd581bb420c2949674383d97d4a2d3
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2024-07-04 09:48:00 +0200

    fix prototype reutrning

--- nanojq.js
+++ nanojq.js
@@ -3,47 +3,51 @@
     if (!(this instanceof NanoJQ)) {
       return new NanoJQ(selector);
     }
+
     this.elements =
       typeof selector === "string"
-        ? document.querySelectorAll(selector)
-        : [].concat(selector);
-    return this.elements.length === 1 ? this.elements[0] : this.elements;
+        ? Array.from(document.querySelectorAll(selector))
+        : Array.isArray(selector)
+        ? selector
+        : [selector];
+
+    return this;
   }
 
   NanoJQ.prototype = {
     hide: function () {
-      this.elements.forEach((el) => (el.style.display = "none"));
+      this.elements.forEach((el) => {
+        if (el && el.style) el.style.display = "none";
+      });
       return this;
     },
     show: function () {
-      this.elements.forEach((el) => (el.style.display = ""));
+      this.elements.forEach((el) => {
+        if (el && el.style) el.style.display = "";
+      });
       return this;
     },
     on: function (event, callback) {
-      this.elements.forEach((el) => el.addEventListener(event, callback));
+      this.elements.forEach((el) => {
+        if (el && el.addEventListener) el.addEventListener(event, callback);
+      });
       return this;
     },
     attr: function (name, value) {
       if (value === undefined) {
         // Getter
-        return this.elements[0].getAttribute(name);
+        return this.elements[0] && this.elements[0].getAttribute
+          ? this.elements[0].getAttribute(name)
+          : null;
       } else {
         // Setter
-        this.elements.forEach((el) => el.setAttribute(name, value));
+        this.elements.forEach((el) => {
+          if (el && el.setAttribute) el.setAttribute(name, value);
+        });
         return this;
       }
     },
   };
-
-  // Extend element prototypes
-  Element.prototype.hide = NanoJQ.prototype.hide;
-  Element.prototype.show = NanoJQ.prototype.show;
-  Element.prototype.on = NanoJQ.prototype.on;
-  Element.prototype.attr = NanoJQ.prototype.attr;
-  NodeList.prototype.hide = NanoJQ.prototype.hide;
-  NodeList.prototype.show = NanoJQ.prototype.show;
-  NodeList.prototype.on = NanoJQ.prototype.on;
-  NodeList.prototype.attr = NanoJQ.prototype.attr;
 
   window.$ = NanoJQ;
   window._ = console.log.bind(console);
